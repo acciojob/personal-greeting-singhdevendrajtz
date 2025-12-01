48d67297e6e712b8f041525bd7886e37ce4b63d5
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-12-01 14:59:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [output,ouput_function]=useState("");
+  const handleChange = (event) => {
+      ouput_function(event.target.value); // Update the state with the new input value
+    };
   return (
     <div>
-        {/* Do not remove the main div */}
+       <input onChange={handleChange}></input> 
+      {output!=""?<p>Hello, {output}!</p>:output=output}
     </div>
   )
 }
