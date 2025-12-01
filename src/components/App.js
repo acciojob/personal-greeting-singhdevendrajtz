96cc537740e533cb05e3bb3b637cdbea889ae56f
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-12-01 15:16:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
-
-import React, { useState } from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let [output,ouput_function]=useState("");
-  const handleChange = (event) => {
-      ouput_function(event.target.value); // Update the state with the new input value
-    };
+  const [name,setName ] = useState('')
   return (
     <div>
-    <p>Enter your name:</p>
-       <input onChange={handleChange} value={output}></input> 
-      {output!=""?<p>Hello, {output}!</p>:null}
+        {/* Do not remove the main div */}
+     <p>Enter your name:</p>
+     <input
+       type="text"
+       value={name}
+       onChange={(e) => setName(e.target.value)}
+     />
+     {name ? <p>Hello {name}!</p> : null}
     </div>
   )
 }
